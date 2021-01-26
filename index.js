f135e35392d7a79ada9a10263769ccb7ccbf24c9
James Ballard <jamesballard901@gmail.com>
2021-01-26 11:36:09 -0600

    server running

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const {PrismaClient} = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const path = require('path');
+const morgan = require('morgan');
+const csurf = require('csurf');
 
 const prisma = new PrismaClient();
 const app = express();
 const port = 5000;
 
-app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
-(async () => {
-  let type = await prisma.set.findFirst({
-            where: {
-              name: 'Kaldheim',
-            },
-          });
-  console.log(type)
-  await prisma.$disconnect();
-})();
+app.use(cors({ origin: true }));
+app.use(helmet({ hsts: false }));
+app.use(csurf({
+  cookie: {
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production',
+    httpOnly: true,
+  }
+}));
 
-const server = app.listen(port, () => 
-  console.log(`Server ready at: http://localhost:${port}`))
+
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join("client/build")));
+//   app.get(/\/(?!api)*/, (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
+
+// app.use(function(_req, _res, next) {
+//   next(createError(404));
+// });
+
+// app.use(function(err, _req, res, _next) {
+//   res.status(err.status || 500);
+//   if (err.status === 401) {
+//     res.set('WWW-Authenticate', 'Bearer');
+//   }
+//   res.json({
+//     message: err.message,
+//     error: JSON.parse(JSON.stringify(err)),
+//   });
+// });
+
+module.exports = app;
